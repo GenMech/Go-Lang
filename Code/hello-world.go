747e23a4db067ad446e7afeccdd202f96fdb0a4c
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-02 23:56:46 +0530

    Small info

--- Code/hello-world.go
+++ Code/hello-world.go
@@ -5,3 +5,5 @@ import "fmt"
 func main() {
     fmt.Println("hello world")
 }
+
+//The Go fmt package supports two closely-related functions for formatting a string to be displayed on the terminal. Print() accepts strings as arguments and concatenates them without any spacing. . Println() , on the other hand, adds a space between strings and appends a new line to the concatenated output string
