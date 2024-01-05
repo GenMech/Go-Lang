1ac9762649d5fc148b508dceec84416b062c8598
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-05 23:55:01 +0530

    Hashmap basic

--- Code/fundamentals.go
+++ Code/fundamentals.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"maps"
+)
 
 func main() {
     fmt.Println("hello world")
-    basic()
+    basic() // for loop basics
 	array() // basic of arrays
+	hashmap() // hashmaps basics
 }
 
 //The Go fmt package supports two closely-related functions for formatting a string to be displayed on the terminal. Print() accepts strings as arguments and concatenates them without any spacing. . Println() , on the other hand, adds a space between strings and appends a new line to the concatenated output string
@@ -28,4 +32,40 @@ func basic(){
 		break
 	}
 
+}
+
+// Hashmap in GO
+func hashmap() {
+
+    m := make(map[string]int)
+
+    m["k1"] = 7
+    m["k2"] = 13
+
+    fmt.Println("map:", m)
+
+    v1 := m["k1"]
+    fmt.Println("v1:", v1)
+
+    v3 := m["k3"]
+    fmt.Println("v3:", v3)
+
+    fmt.Println("len:", len(m))
+
+    delete(m, "k2")
+    fmt.Println("map:", m)
+
+    clear(m)
+    fmt.Println("map:", m)
+
+    _, prs := m["k2"]
+    fmt.Println("prs:", prs)
+
+    n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
+
+    n2 := map[string]int{"foo": 1, "bar": 2}
+    if maps.Equal(n, n2) {
+        fmt.Println("n == n2")
+    }
 }
