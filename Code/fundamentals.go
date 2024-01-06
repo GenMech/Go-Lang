e99f250892380d834d66dab8deeaef40146c5edc
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-06 23:35:59 +0530

    Ranges in Go

--- Code/fundamentals.go
+++ Code/fundamentals.go
@@ -10,6 +10,7 @@ func main() {
     basic() // for loop basics
 	array() // basic of arrays
 	hashmap() // hashmaps basics
+    ranges() // for ranges
 }
 
 //The Go fmt package supports two closely-related functions for formatting a string to be displayed on the terminal. Print() accepts strings as arguments and concatenates them without any spacing. . Println() , on the other hand, adds a space between strings and appends a new line to the concatenated output string
@@ -58,7 +59,7 @@ func hashmap() {
     clear(m)
     fmt.Println("map:", m)
 
-    _, prs := m["k2"]
+    _, prs := m["k2"] // Here,we are using the blank identifier (_) to ignore the first value returned by the expression m["k2"]. This is commonly used when you are interested in checking the presence of a key in a map but don't need the actual value associated with that key.
     fmt.Println("prs:", prs)
 
     n := map[string]int{"foo": 1, "bar": 2}
@@ -71,3 +72,32 @@ func hashmap() {
 }
 
 //The _ (underscore) is known as the blank identifier in Go. We can use the blank identifier to declare and use the unused variables. The unused variables are the variables that are only defined once in the program and not used. 
+
+// Ranges in Go
+func ranges(){
+    nums := []int{2, 3, 4}
+    sum := 0
+    for _, num := range nums { // Here we dont want index so we ignored it
+        sum += num
+    }
+    fmt.Println("sum:", sum)
+
+    for i, num := range nums {
+        if num == 3 {
+            fmt.Println("index:", i)
+        }
+    }
+
+    kvs := map[string]string{"a": "apple", "b": "banana"}
+    for k, v := range kvs {
+        fmt.Printf("%s -> %s\n", k, v)
+    }
+
+    for k := range kvs {
+        fmt.Println("key:", k)
+    }
+
+    for i, c := range "go" {
+        fmt.Println(i, c)
+    }
+}
