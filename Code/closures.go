96979bb1051c7f9616137c8f329167a95f2b03b1
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-10 00:13:29 +0530

    Closures in Go - 2

--- Code/closures.go
+++ Code/closures.go
@@ -13,12 +13,23 @@ func intSeq() func() int {
 
 func closures() {
 
+	// Creating a closure by calling intSeq()
     nextInt := intSeq()
 
+	fmt.Println("Result:", nextInt())
+
+	// Invoking the closure and printing the result
     fmt.Println(nextInt())
     fmt.Println(nextInt())
     fmt.Println(nextInt())
 
+	// Creating a new closure with independent state
     newInts := intSeq()
+
+	// 
     fmt.Println(newInts())
 }
+
+// Here, intSeq() is a function call, and it returns another function. The returned function is assigned to the variable nextInt. So, nextInt is indeed a variable, but it's a variable that holds a function.
+
+// In Go, functions are first-class citizens, meaning they can be assigned to variables, passed as arguments to other functions, and returned from functions. The concept of closures allows functions to capture and carry the surrounding state with them, which is what seems to be happening in your example.
