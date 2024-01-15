1efeb42ee2f0e387daaaa61d23d1d2a050f2820b
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-15 22:47:52 +0530

    Struct - 2

--- Code/fundamentals.go
+++ Code/fundamentals.go
@@ -15,7 +15,8 @@ func main() {
     // recusion() // recursion in Go
     // closures()
     // pointers()
-    methods()
+    structExample()
+    // methods()
 }
 
 //The Go fmt package supports two closely-related functions for formatting a string to be displayed on the terminal. Print() accepts strings as arguments and concatenates them without any spacing. . Println() , on the other hand, adds a space between strings and appends a new line to the concatenated output string
