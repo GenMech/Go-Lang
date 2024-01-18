eee136f456b9b10e23a231919dd4dc87efb67f37
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-01-19 00:09:31 +0530

    Channels in Go

--- Code/fundamentals.go
+++ Code/fundamentals.go
@@ -15,8 +15,9 @@ func main() {
     // recusion() // recursion in Go
     // closures()
     // pointers()
-    structExample()
+    // structExample()
     // methods()
+    channel()
 }
 
 //The Go fmt package supports two closely-related functions for formatting a string to be displayed on the terminal. Print() accepts strings as arguments and concatenates them without any spacing. . Println() , on the other hand, adds a space between strings and appends a new line to the concatenated output string
