0f9d9f0f726bb9f0100ea7b60eb245c3696ab7e9
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-02-02 23:58:36 +0530

    Channel in Go

--- Code/channel.go
+++ Code/channel.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func channel() {
 
-    messages := make(chan string)
+	messages := make(chan string) // Create a new channel with make(chan val-type). Channels are typed by the values they convey.
 
-    go func() { messages <- "ping" }()
+	go func() { messages <- "ping" }() //Create a new channel with make(chan val-type). Channels are typed by the values they convey.
 
-    msg := <-messages
-    fmt.Println(msg)
+	msg := <-messages //The <-channel syntax receives a value from the channel. Here we’ll receive the "ping" message we sent above and print it out.
+	fmt.Println(msg)
 }
+
+// Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
