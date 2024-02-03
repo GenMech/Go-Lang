03362846fa3a2cc49e920b46659077de2c9a4dd8
Gitesh Pareek <pareekgitesh89@gmail.com>
2024-02-03 23:59:38 +0530

    Channel in Go - 2

--- Code/channel.go
+++ Code/channel.go
@@ -13,3 +13,9 @@ func channel() {
 }
 
 // Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+
+// By default channels are unbuffered, meaning that they will only accept sends (chan <-) if there is a corresponding receive (<- chan) ready to receive the sent value. Buffered channels accept a limited number of values without a corresponding receiver for those values.
+
+// Go channels are used for communicating between concurrently running functions by sending and receiving a specific element type's data. When we have numerous Goroutines running at the same time, channels are the most convenient way for them to communicate with one another.
+
+// Goroutines provide a means for multiprocessing in a Golang application, allowing multiple processes to run simultaneously. In addition, Channels and WaitGroups enable passing data between threads or blocking one thread until another completes.
